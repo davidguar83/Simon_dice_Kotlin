b5214b6115b011052a15300c7053abbbdc0de9d9
david <davidrguardiola@hotmail.es>
2020-12-18 20:40:59 +0100

    modificaciones

--- app/src/main/java/com/dam2david/simon_dice/MainActivity.kt
+++ app/src/main/java/com/dam2david/simon_dice/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 package com.dam2david.simon_dice
 
 import android.os.Bundle
@@ -36,21 +38,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//creo mensaje de texto y lo meto en una toast
         val text = getString(R.string.mensajeToast1)
         val toast= Toast.makeText(applicationContext,text,declaracion)
 
 
 
-
+//sirve para cuando presiones el boton empezar se inicie la ronda de la maquina seguido del mensaje de la toas y por ultimo empieza el turno jugador
         val rollButton: Button = findViewById(R.id.mi_boton)
         rollButton.setOnClickListener {
            rondaOrdenador()
             toast.show()
+            turnoJugador()
 
         }
 
-
+//sirve para cuando presiones el boton haga lo del metoso limpiar lista()
         val comprobarBoton: Button = findViewById(R.id.mi_boton2)
         comprobarBoton.setOnClickListener{
 
@@ -58,7 +61,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
+//comprueva las dos listas en caso de estar correctas , incrementa en +1 las ronda
         val comprobarRonda: Button = findViewById(R.id.button3)
         comprobarRonda.setOnClickListener {
 
@@ -66,6 +69,17 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+
+
+
+
+
+        diceImage = findViewById(R.id.imagen_rgb)
+    }
+
+// metodo para guardar en la lista jugador los colores o mas vien los numeros, para despues compararlos con la lista ordenador
+    private fun turnoJugador(){
 
         val botonVerde: Button = findViewById(R.id.buttonverde)
         botonVerde.setOnClickListener {
@@ -90,9 +104,9 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        diceImage = findViewById(R.id.imagen_rgb)
-    }
 
+    }
+//metodo para limpiar las listas tanto jugador como maquina, no mantiene la dinamica original del juego, pero es una posible variable curiosa, ya que limpia las listas creando el juego de nuevo pero con una ronda mas de color, sin mantener el color inicial
     private fun comprobarResultado() {
 
         val text2 = getString(R.string.mensajeToast2)
@@ -104,9 +118,9 @@ class MainActivity : AppCompatActivity() {
 
             toast2.show()
 
-            rondas++;
-            //limpiarListas()
-            listajugador.value?.clear()
+            rondas++
+            limpiarListas()
+            //listajugador.value?.clear()
             rondaOrdenador()
 
         } else {
@@ -120,7 +134,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+//metodo que limpia las dos listasmutables
     private fun limpiarListas(){
 
         listaordenador.value?.clear()
@@ -131,13 +145,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+// metodo que genera una combinacion de numeros que se almacenan en una lista mutable, cambia cada numero con una imagen que equivale a un color en concreto
     private fun rondaOrdenador() {
 
 
-
+//abria que hacer una coroutina para que los colores se ejecutaran con una diferencia de tiempo, para poder ser vistos por el ojo humano
         for (i in 1..rondas)  {
-            val randomInt = Random.nextInt(4) + 1
+            val randomInt = (1+Random.nextInt(4) )
 
             listaordenador.value?.add(randomInt)
             listaordenador.postValue(listaordenador.value)
